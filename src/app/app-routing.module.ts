8bf7c2dbe105e7fe9239822a9bcf748c6f3bcdb2
Aastik Sharma <aastiksharma1208@gmail.com>
2025-07-22 13:25:24 +0530

    Add the routing for the about and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Todos } from './MyComponents/todos/todos';
+import { About } from './MyComponents/about/about';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: Todos },
+  { path: 'about', component: About }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
+import { AppRoutingModule } from './app-routing.module';
 import { App } from './app';
 import { Todos } from './MyComponents/todos/todos';
 import { TodoItem } from './MyComponents/todo-item/todo-item';
 import { AddToDo } from './MyComponents/add-to-do/add-to-do';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { About } from './MyComponents/about/about';
 
 
 @NgModule({
   declarations: [
     App,
     Todos,
-    AddToDo
+    AddToDo,
+    About
   ],
   imports: [
     BrowserModule,
